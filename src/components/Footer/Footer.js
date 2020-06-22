638f96d2bb2690731f958544050d86af12b52b22
bairdh <hrbaird10@gmail.com>
2020-06-22 09:56:57 -0500

    added comments to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './Footer.css'
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
+// changed by Heather
 class Footer extends Component{
   render(){
     return(
