f70826111fb5ddad299e2d26ce0d73e60066fcda
Shaokee Moua <m.shaokee@gmail.com>
2020-06-23 14:24:46 -0500

    result view date selector updated

--- src/components/LandingPage/ResultPage.jsx
+++ src/components/LandingPage/ResultPage.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //MATERIAL UI
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, Typography, TextField, Box, Button } from '@material-ui/core';
+import { Grid, Typography, TextField, Box, Button, FormControl, MenuItem, Select, InputLabel, Input, TypeDate } from '@material-ui/core';
 // PropTypes allows us to import style.jsx for use
 import PropTypes from 'prop-types';
 import styles from '../Style/Style';
 
 class ResultPage extends Component {
 
-    componentDidMount(){
+    state = {
+        open: false,
+        month: '',
+        type: '',
+        attendance: '',
+        sponsorshipCost: '',
+        income: ''
+    };//end state
+
+    componentDidMount() {
         console.log('ResultPage has been MOUNTED');
     };//end componentDidMount
-    
+
     render() {
         // allows us to connect this.props to styles 
         const { classes } = this.props;
@@ -26,21 +35,34 @@ class ResultPage extends Component {
                     </Grid>
                     {/* section that holds the advanced search filters */}
                     <Box className={classes.box_grey}>
-                        {/* first 3 inputs */}
-                        <Grid container justify="center" spacing={2}>
-                            <Grid item md={12}><Typography className={classes.landSearchTitle}>Results View</Typography></Grid>
-                            <Grid item xs={12} md={3}><TextField fullWidth={true} label="Location" /></Grid>
-                            <Grid item xs={12} md={3}><TextField fullWidth={true} label="Month" /></Grid>
-                            <Grid item xs={12} md={3}><TextField fullWidth={true} label="Type" /></Grid>
-                            <Grid/>
-                            {/* second batch of inputs */}
+                        <Grid justify="center" spacing={2}>
+                            <Grid item xs={12} md={12}><Typography className={classes.landSearchTitle}>Results View</Typography></Grid>
+                            {/* location and month selector options */}
                             <Grid container justify="center" spacing={2}>
-                                <Grid item xs={12} md={3}><TextField fullWidth={true} label="Attendance" /></Grid>
-                                <Grid item xs={12} md={3}><TextField fullWidth={true} label="Sponsorship Cost" /></Grid>
-                                <Grid item xs={12} md={3}><TextField fullWidth={true} label="Household Income" /></Grid>
+                                <Grid item xs={12} md={5}><TextField fullWidth={true} label="Location" /></Grid>
+                                {/* month selector */}
+                                <Grid item xs={12} md={3}>
+                                    <InputLabel>start date</InputLabel>
+                                    <TextField type="date" />
+                                </Grid>
+                                <Grid item xs={12} md={2}>
+                                    <InputLabel>end date</InputLabel>
+                                    <TextField type="date" />
+                                </Grid>
                             </Grid>
-                            <Grid container justify="center">
-                            <Grid item xs={12} md={1}><Button className={classes.filterBtn} variant="outlined">Filter</Button></Grid>
+                            {/* type and attendance selector options*/}
+                            <Grid container justify="center" spacing={2}>
+                                <Grid item xs={12} md={5}><TextField fullWidth={true} label="Type" /></Grid>
+                                <Grid item xs={12} md={5}><TextField fullWidth={true} label="Attendance" /></Grid>
+                            </Grid>
+                            {/* sponsorship cost and household income options */}
+                            <Grid container justify="center" spacing={2}>
+                                <Grid item xs={12} md={5}><TextField fullWidth={true} label="Sponsorship Cost" /></Grid>
+                                <Grid item xs={12} md={5}><TextField fullWidth={true} label="Household Income" /></Grid>
+                            </Grid>
+                            {/* button grid that centers it */}
+                            <Grid container justify="center" spacing={2}>
+                                <Grid item xs={12} md={1}><Button className={classes.filterBtn} variant="outlined">Filter</Button></Grid>
                             </Grid>
                         </Grid>
                     </Box>
