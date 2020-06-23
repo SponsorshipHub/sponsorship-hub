8d1beb292382fb24d95f9e9767e4ce92570d42fc
Tan Nguyen <tannguyen1024@gmail.com>
2020-06-23 16:54:02 -0500

    Updated database

--- database.sql
+++ database.sql
@@ -494,7 +494,7 @@ WHERE gender='female' AND junction_event_gender.percentage >= 30
     AND city = 'St. Paul';
 
 -- Filter by Location, Month, Type, Attendance, Sponsorship Cost, and Household Income -- SUPER FILTER --
-SELECT *
+SELECT event.id, event_name, start_date, end_date, city, state, event_image_url
 FROM "event"
     JOIN venues ON venues.id=event.venue_id
     JOIN sponsorships ON event.id=sponsorships.event_id
@@ -510,53 +510,54 @@ WHERE state ILIKE '%Minnesota%' -- State
 -- Income Range (50000-75999) is 20%
 
 ---- GET ALL EVENTS
-SELECT * FROM "event";
+SELECT *
+FROM "event";
 
 ---- SELECT * WHERE EVENT.ID = $1
-SELECT e.*, et.type, v.*, 
-json_agg(DISTINCT jsonb_build_object('sponsorship_id', s.id, 'sponsor_name', s.sponsor_name, 'sponsor_price', s.sponsor_price, 'sponsor_image_url', s.sponsor_image_url, 'sponsor_description', s.sponsor_description)) AS sponsorship,
-json_agg(DISTINCT jsonb_build_object('age_range_id', jea.age_range_id, 'age_percentage', jea.percentage, 'age_range', ar.age_range)) AS age, 
-json_agg(DISTINCT jsonb_build_object('gender_id', g.id, 'gender', g.gender, 'gender_percentage', jeg.percentage)) AS gender, 
-json_agg(DISTINCT jsonb_build_object('income_range_id', ir.id, 'income_range', ir.income_range, 'income_percentage', jei.percentage)) AS income, 
-json_agg(DISTINCT jsonb_build_object('residency_id', r.id, 'residency', r.residency, 'residency_percentage', jer.percentage)) AS residency
+SELECT e.*, et.type, v.*,
+    json_agg(DISTINCT jsonb_build_object('sponsorship_id', s.id, 'sponsor_name', s.sponsor_name, 'sponsor_price', s.sponsor_price, 'sponsor_image_url', s.sponsor_image_url, 'sponsor_description', s.sponsor_description)) AS sponsorship,
+    json_agg(DISTINCT jsonb_build_object('age_range_id', jea.age_range_id, 'age_percentage', jea.percentage, 'age_range', ar.age_range)) AS age,
+    json_agg(DISTINCT jsonb_build_object('gender_id', g.id, 'gender', g.gender, 'gender_percentage', jeg.percentage)) AS gender,
+    json_agg(DISTINCT jsonb_build_object('income_range_id', ir.id, 'income_range', ir.income_range, 'income_percentage', jei.percentage)) AS income,
+    json_agg(DISTINCT jsonb_build_object('residency_id', r.id, 'residency', r.residency, 'residency_percentage', jer.percentage)) AS residency
 FROM event AS e
---TYPE JUNCITON
-JOIN junction_event_type AS jet
-ON e.id = jet.event_id
---TYPE
-JOIN event_type AS et
-ON jet.type_id = et.id
---VENUE
-JOIN venues AS v
-ON e.venue_id = v.id
---AGE RANGE JUNCTION
-JOIN junction_event_age AS jea
-ON e.id = jea.event_id
---AGE RANGE
-JOIN age_range AS ar
-ON jea.age_range_id = ar.id
---GENDER JUNCTION
-JOIN junction_event_gender AS jeg
-ON e.id = jeg.event_id
---GENDER
-JOIN gender AS g
-ON jeg.gender_id = g.id
---INCOME JUNCTION
-JOIN junction_event_income AS jei
-ON e.id = jei.event_id
---INCOME RANGE
-JOIN income_range AS ir
-ON jei.income_range_id = ir.id
---LOCTION JUNCTION
-JOIN junction_event_residency AS jer
-ON e.id = jer.event_id
---LOCTION
-JOIN residency AS r
-ON jer.residency_id = r.id
---SPONSORSHIP
-JOIN sponsorships AS s
-ON e.id = s.event_id
+    --TYPE JUNCITON
+    JOIN junction_event_type AS jet
+    ON e.id = jet.event_id
+    --TYPE
+    JOIN event_type AS et
+    ON jet.type_id = et.id
+    --VENUE
+    JOIN venues AS v
+    ON e.venue_id = v.id
+    --AGE RANGE JUNCTION
+    JOIN junction_event_age AS jea
+    ON e.id = jea.event_id
+    --AGE RANGE
+    JOIN age_range AS ar
+    ON jea.age_range_id = ar.id
+    --GENDER JUNCTION
+    JOIN junction_event_gender AS jeg
+    ON e.id = jeg.event_id
+    --GENDER
+    JOIN gender AS g
+    ON jeg.gender_id = g.id
+    --INCOME JUNCTION
+    JOIN junction_event_income AS jei
+    ON e.id = jei.event_id
+    --INCOME RANGE
+    JOIN income_range AS ir
+    ON jei.income_range_id = ir.id
+    --LOCTION JUNCTION
+    JOIN junction_event_residency AS jer
+    ON e.id = jer.event_id
+    --LOCTION
+    JOIN residency AS r
+    ON jer.residency_id = r.id
+    --SPONSORSHIP
+    JOIN sponsorships AS s
+    ON e.id = s.event_id
 WHERE e.id = 1
-GROUP BY e.id, v.id, et.type; 
+GROUP BY e.id, v.id, et.type;
 SELECT *
 FROM "event";
