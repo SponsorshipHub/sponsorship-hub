27f5d58492fb3184e186ae4e53cba1b117c62819
Tan Nguyen <tannguyen1024@gmail.com>
2020-06-22 23:58:30 -0500

    Updated CreateEvent.jsx

--- src/components/Style/Style.jsx
+++ src/components/Style/Style.jsx
@@ -1,6 +1,12 @@
 const styles = {
     btn_def: {
         background: 'grey'
+    },
+    box_grey: {
+        background: '#ECECEC',
+    },
+    margin: {
+        margin: '20px'
     }
 };//end styles
 
