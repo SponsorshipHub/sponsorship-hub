1c75aa5adf20ab6a263d4025225f5104cd5e53b6
Alan Henderson <mr.alan.henderson@gmail.com>
2020-06-30 12:57:05 -0500

    demographics are now created as soon as a new event is created

--- server/routes/demo.router.js
+++ server/routes/demo.router.js
@@ -12,42 +12,26 @@ const { rejectLevel2 } = require('../modules/auth_lvl_2'); // Rejects level 2 an
  */
 //gender
 //original post route
-router.post('/gender', rejectUnauthenticated, rejectLevel1, (req, res) => {
-    console.log('in gender demo POST req.body:', req.body);
-    const queryText = `INSERT INTO junction_event_gender 
-    (event_id, gender_id, percentage) 
-    VALUES ($1, 1, $2), ($1, 2, $3), ($1, 3, $4);`;
-    pool.query(queryText, [req.body.event_id, req.body.female, req.body.male, req.body.other])
-     .then(result => {
-         res.sendStatus(200);
-     }).catch (error => {
-         console.log('post gender Demo route has error', error);
-         res.sendStatus(500);
-     })
-});
-//attempt to move demo create into event create
 // router.post('/gender', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //     console.log('in gender demo POST req.body:', req.body);
 //     const queryText = `INSERT INTO junction_event_gender 
 //     (event_id, gender_id, percentage) 
-//     VALUES ($1, 1, 0), ($1, 2, 0), ($1, 3, 0;`;
-//     pool.query(queryText, [req.body.event_id])
-//         .then(result => {
-//             res.sendStatus(200);
-//         }).catch(error => {
-//             console.log('post gender Demo route has error', error);
-//             res.sendStatus(500);
-//         })
+//     VALUES ($1, 1, $2), ($1, 2, $3), ($1, 3, $4);`;
+//     pool.query(queryText, [req.body.event_id, req.body.female, req.body.male, req.body.other])
+//      .then(result => {
+//          res.sendStatus(200);
+//      }).catch (error => {
+//          console.log('post gender Demo route has error', error);
+//          res.sendStatus(500);
+//      })
 // });
-
-//income
-//original post route
-router.post('/income', rejectUnauthenticated, rejectLevel1, (req, res) => {
-    console.log('in income demo POST req.body:', req.body);
-    const queryText = `INSERT INTO junction_event_income
-    (event_id, income_range_id, percentage)
-    VALUES ($1, 1, $2), ($1, 2, $3), ($1, 3, $4), ($1, 4, $5), ($1, 5, $6), ($1, 6, $7), ($1, 7, $8);`;
-    pool.query(queryText, [req.body.event_id, req.body.Income0_24999, req.body.Income25000_49999, req.body.Income50000_74999, req.body.Income75000_99999, req.body.Income100000_149999, req.body.Income150000_199999, req.body.Income200000,])
+//attempt to move demo create into event create
+router.post('/gender', rejectUnauthenticated, rejectLevel1, (req, res) => {
+    console.log('in gender demo POST req.body:', req.body.event_id);
+    const queryText = `INSERT INTO junction_event_gender 
+    (event_id, gender_id, percentage) 
+    VALUES ($1, 1, 0), ($1, 2, 0), ($1, 3, 0);`;
+    pool.query(queryText, [req.body.event_id])
         .then(result => {
             res.sendStatus(200);
         }).catch(error => {
@@ -55,13 +39,15 @@ router.post('/income', rejectUnauthenticated, rejectLevel1, (req, res) => {
             res.sendStatus(500);
         })
 });
-//attempt to move demo create into event create
+
+//income
+//original post route
 // router.post('/income', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //     console.log('in income demo POST req.body:', req.body);
 //     const queryText = `INSERT INTO junction_event_income
 //     (event_id, income_range_id, percentage)
-//     VALUES ($1, 1, 0), ($1, 2, 0), ($1, 3, 0), ($1, 4, 0), ($1, 5, 0), ($1, 6, 0), ($1, 7, 0);`;
-//     pool.query(queryText, [req.body.event_id])
+//     VALUES ($1, 1, $2), ($1, 2, $3), ($1, 3, $4), ($1, 4, $5), ($1, 5, $6), ($1, 6, $7), ($1, 7, $8);`;
+//     pool.query(queryText, [req.body.event_id, req.body.Income0_24999, req.body.Income25000_49999, req.body.Income50000_74999, req.body.Income75000_99999, req.body.Income100000_149999, req.body.Income150000_199999, req.body.Income200000,])
 //         .then(result => {
 //             res.sendStatus(200);
 //         }).catch(error => {
@@ -69,15 +55,13 @@ router.post('/income', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //             res.sendStatus(500);
 //         })
 // });
-
-//age
-//original post route
-router.post('/age', rejectUnauthenticated, rejectLevel1, (req, res) => {
-    console.log('in age demo POST req.body:', req.body);
-    const queryText = `INSERT INTO junction_event_age
-    (event_id, age_range_id, percentage)
-     VALUES ($1, 1, $2), ($1, 2, $3), ($1, 3, $4), ($1, 4, $5), ($1, 5, $6), ($1, 6, $7), ($1, 7, $8);`;
-    pool.query(queryText, [req.body.event_id, req.body.Age0_17, req.body.Age18_24, req.body.Age25_34, req.body.Age35_44, req.body.Age45_54, req.body.Age55_64, req.body.Age65])
+//attempt to move demo create into event create
+router.post('/income', rejectUnauthenticated, rejectLevel1, (req, res) => {
+    console.log('in income demo POST req.body:', req.body);
+    const queryText = `INSERT INTO junction_event_income
+    (event_id, income_range_id, percentage)
+    VALUES ($1, 1, 0), ($1, 2, 0), ($1, 3, 0), ($1, 4, 0), ($1, 5, 0), ($1, 6, 0), ($1, 7, 0);`;
+    pool.query(queryText, [req.body.event_id])
         .then(result => {
             res.sendStatus(200);
         }).catch(error => {
@@ -86,13 +70,14 @@ router.post('/age', rejectUnauthenticated, rejectLevel1, (req, res) => {
         })
 });
 
-//attempt to move demo create into event create
+//age
+//original post route
 // router.post('/age', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //     console.log('in age demo POST req.body:', req.body);
 //     const queryText = `INSERT INTO junction_event_age
 //     (event_id, age_range_id, percentage)
-//      VALUES ($1, 1, 0), ($1, 2, 0), ($1, 3, 0), ($1, 4, 0), ($1, 5, 0), ($1, 6, 0), ($1, 7, 0);`;
-//     pool.query(queryText, [req.body.event_id])
+//      VALUES ($1, 1, $2), ($1, 2, $3), ($1, 3, $4), ($1, 4, $5), ($1, 5, $6), ($1, 6, $7), ($1, 7, $8);`;
+//     pool.query(queryText, [req.body.event_id, req.body.Age0_17, req.body.Age18_24, req.body.Age25_34, req.body.Age35_44, req.body.Age45_54, req.body.Age55_64, req.body.Age65])
 //         .then(result => {
 //             res.sendStatus(200);
 //         }).catch(error => {
@@ -101,14 +86,13 @@ router.post('/age', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //         })
 // });
 
-//residency
-//original post route
-router.post('/resident', rejectUnauthenticated, rejectLevel1, (req, res) => {
-    console.log('in resident demo POST req.body:', req.body);
-    const queryText = `INSERT INTO junction_event_residency 
-    (event_id, residency_id, percentage) 
-    VALUES ($1, 1, $2), ($1, 2, $3);`;
-    pool.query(queryText, [req.body.event_id, req.body.in_state, req.body.out_of_state])
+//attempt to move demo create into event create
+router.post('/age', rejectUnauthenticated, rejectLevel1, (req, res) => {
+    console.log('in age demo POST req.body:', req.body);
+    const queryText = `INSERT INTO junction_event_age
+    (event_id, age_range_id, percentage)
+     VALUES ($1, 1, 0), ($1, 2, 0), ($1, 3, 0), ($1, 4, 0), ($1, 5, 0), ($1, 6, 0), ($1, 7, 0);`;
+    pool.query(queryText, [req.body.event_id])
         .then(result => {
             res.sendStatus(200);
         }).catch(error => {
@@ -117,13 +101,14 @@ router.post('/resident', rejectUnauthenticated, rejectLevel1, (req, res) => {
         })
 });
 
-//attempt to move demo create into event create
+//residency
+//original post route
 // router.post('/resident', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //     console.log('in resident demo POST req.body:', req.body);
 //     const queryText = `INSERT INTO junction_event_residency 
 //     (event_id, residency_id, percentage) 
-//     VALUES ($1, 1, 0), ($1, 2, 0);`;
-//     pool.query(queryText, [req.body.event_id])
+//     VALUES ($1, 1, $2), ($1, 2, $3);`;
+//     pool.query(queryText, [req.body.event_id, req.body.in_state, req.body.out_of_state])
 //         .then(result => {
 //             res.sendStatus(200);
 //         }).catch(error => {
@@ -131,6 +116,21 @@ router.post('/resident', rejectUnauthenticated, rejectLevel1, (req, res) => {
 //             res.sendStatus(500);
 //         })
 // });
+
+//attempt to move demo create into event create
+router.post('/resident', rejectUnauthenticated, rejectLevel1, (req, res) => {
+    console.log('in resident demo POST req.body:', req.body);
+    const queryText = `INSERT INTO junction_event_residency 
+    (event_id, residency_id, percentage) 
+    VALUES ($1, 1, 0), ($1, 2, 0);`;
+    pool.query(queryText, [req.body.event_id])
+        .then(result => {
+            res.sendStatus(200);
+        }).catch(error => {
+            console.log('post gender Demo route has error', error);
+            res.sendStatus(500);
+        })
+});
 
 /**
  * PUT routes gender, income, age, residency
